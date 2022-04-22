3108cb9ec5fa2867c37196492f0d89763b8fd9bb
Olawale <stwalez1@gmail.com>
2022-04-22 17:07:43 +0000

    updated main.tf to call other regions

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@ output "us_alb_hostname_out" {
 }
 
 
-# output "uk_alb_hostname_out" {
-#   value = module.common_uk.alb_hostname
-# }
+output "uk_alb_hostname_out" {
+  value = module.common_uk.alb_hostname
+}
 
-# output "germany_alb_hostname_out" {
-#   value = module.common_germany.alb_hostname
-# }
+output "germany_alb_hostname_out" {
+  value = module.common_germany.alb_hostname
+}
