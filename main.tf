c6a26998ce787ba35f1152224a14e0bcecfca0d0
Olawale <stwalez1@gmail.com>
2022-04-22 16:54:41 +0000

    updated main.tf to call other regions

--- main.tf
+++ main.tf
@@ -5,22 +5,22 @@ module "common_us" {
   cidr_block = var.cidr_block
 }
 
-# module "common_uk" {
-#     source = "./common"
-#     aws_region = var.aws_region[1]
-#     providers = {
-#         aws = aws.uk
-#     }
-#     env = var.env
-#     cidr_block = var.cidr_block
-# }
+module "common_uk" {
+    source = "./common"
+    aws_region = var.aws_region[1]
+    providers = {
+        aws = aws.uk
+    }
+    env = var.env
+    cidr_block = var.cidr_block
+}
 
-# module "common_germany" {
-#   source     = "./common"
-#   aws_region = var.aws_region[2]
-#   providers = {
-#     aws = aws.germany
-#   }
-#   env = var.env
-#   cidr_block = var.cidr_block
-# }
+module "common_germany" {
+  source     = "./common"
+  aws_region = var.aws_region[2]
+  providers = {
+    aws = aws.germany
+  }
+  env = var.env
+  cidr_block = var.cidr_block
+}
